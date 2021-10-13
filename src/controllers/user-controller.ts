95c0a11d0b5e26a39b957522017fd9edc8a340f6
KennethAFernandez <kennybf007@gmail.com>
2021-10-12 17:51:20 -0700

    Messing with some things

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -3,7 +3,7 @@ import log from '../config/log';
 import UserSchema from '../model/user-model';
 import mongoose from 'mongoose';
 
-const namespace = 'USER-CONTROLLER'
+const namespace = 'USER-CONTROLLER';
 
 const login = (req: Request, res: Response, next: NextFunction) => {
     let { email, password } = req.body;
@@ -13,21 +13,16 @@ const login = (req: Request, res: Response, next: NextFunction) => {
     .then(users => {
         if (users[0].password === password) {
             log.info(namespace, '[LOGIN SUCCESS]');
-            log.info(namespace, `[EMAIL: ${users[0].email}] [PASSWORD: ${users[0].password}]`);
-            
-            return res.status(200).json({
-                users: users[0].firstName
-            })
+            return res.status(200).json({ users: users[0].firstName })
+
         } else {
             log.info(namespace, '[LOGIN FAIL]');
             res.send('Login failed')
+            // res.sendFile('login.html', { root:'./src/views' })
         }
     })
     .catch((error) => {
-        return res.status(403).json({
-            message: error.message,
-            error
-        });
+        return res.status(403).json({ message: error.message, error });
     });
 }
 
@@ -38,17 +33,11 @@ const getInfo = (req: Request, res: Response, next: NextFunction) => {
     UserSchema.find()
         .exec()
         .then((users) => {
-            return res.status(200).json({
-                users: users,
-                count: users.length
-            });
+            return res.status(200).json({ users: users, count: users.length });
                 
         })
         .catch((error) => {
-            return res.status(500).json({
-                message: error.message,
-                error
-            });
+            return res.status(500).json({ message: error.message, error });
         });
 };
 
@@ -59,8 +48,8 @@ const getInfo = (req: Request, res: Response, next: NextFunction) => {
 
 const createUser =  async (req: Request, res: Response, next: NextFunction) => {
     log.info(namespace, 'CREATING-USER');
-
     let { firstName, lastName, email, password } = req.body;
+
     const user = new UserSchema({
         _id: new mongoose.Types.ObjectId(),
         firstName,
